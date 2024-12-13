dd8e0914e1a24d192b9a7507044804ebcb0a49c8
Elijah Booher <elijahdrake2003@gmail.com>
2024-12-12 23:00:34 -0800

    Added some SQL commands, having issues connecting to server

--- CredentialsDB.sql
+++ CredentialsDB.sql
@@ -1,1 +1,14 @@
-CREATE DATABASE CredentialsDB
+CREATE DATABASE CredentialsDB;
+
+CREATE TABLE account(
+	userID	varChar(30) NOT NULL,
+    password	varChar(30) NOT NULL,
+CONSTRAINT account_PK PRIMARY KEY (userID));
+
+CREATE TABLE passwords(
+	userID      varChar(30) NOT NULL,
+	website		varChar(30) NOT NULL,
+    email		varChar(50),
+    password	varChar(30) NOT NULL,
+
+CONSTRAINT account_FK FOREIGN key (userID) REFERENCES account(userID));
